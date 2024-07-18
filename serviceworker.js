3f296be1248ab8aac7d0d058057f59e44395fce7
svflr <romkaslk2005@gmail.com>
2024-07-19 00:16:54 +0300

    Create serviceworker.js

---
+++ serviceworker.js
@@ -0,0 +1,6 @@
+self.addEventListener(type: 'install', listener: event => {
+    console.log('[SW]: install')
+})
+self.addEventListener(type: 'activate', listener: event => {
+    console.log('[SW]: activate')
+})
