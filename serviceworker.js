275688d1ef44dcc93a212ee1ac694647fe287850
svflr <romkaslk2005@gmail.com>
2024-07-26 00:32:48 +0300

    serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,11 @@
-self.addEventListener(type: 'install', listener: event => {
-    console.log('[SW]: install')
-})
-self.addEventListener(type: 'activate', listener: event => {
-    console.log('[SW]: activate')
-})
+self.addEventListener('install', event => {
+    console.log('[SW]: install');
+});
+
+self.addEventListener('activate', event => {
+    console.log('[SW]: activate');
+});
+
+self.addEventListener('fetch', event => {
+    console.log('[SW]: fetch', event.request.url);
+});
