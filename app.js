485985bec23916e7c75edcd333c745ae5b08a42c
svflr <romkaslk2005@gmail.com>
2024-07-26 00:39:23 +0300

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', async () => {
     if ('serviceWorker' in navigator) {
         try {
-            const reg = await navigator.serviceWorker.register('/serviceworker.js');
+            const reg = await navigator.serviceWorker.register('1234/serviceworker.js');
             console.log('Service worker register success', reg);
         } catch (e) {
             console.log('Service worker register fail', e);
