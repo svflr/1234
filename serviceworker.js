7756ac4c370530acc6f259994a948c4c5bae8669
svflr <romkaslk2005@gmail.com>
2024-07-26 00:50:41 +0300

    serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,16 @@
 self.addEventListener('install', event => {
     console.log('[SW]: install');
+    event.waitUntil(
+        caches.open('v1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/app.js',
+                '/styles.css',
+                '/manifest.json'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('activate', event => {
@@ -8,4 +19,9 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
     console.log('[SW]: fetch', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
